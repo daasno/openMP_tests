27e5bee9dc04e2b04c9165a250fa27d7a4a28c0d
daasno <92796341+daasno@users.noreply.github.com>
2025-09-16 09:57:27 +0100

    Add files via upload

---
+++ pi_openMP_atomic.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <omp.h>
+#include <time.h>
+
+#define N 1e9
+#define W 1.0/N
+
+int main(){
+	
+	double sum = 0.0;
+	double x, pi;
+	double start, end;
+	
+	start = omp_get_wtime();
+	
+	#pragma omp parallel for
+	for(int i=1; i<=(int)N; i++){
+		x = W * (i - 0.5);
+		#pragma omp atomic
+		sum += 4.0/(1.0 + x*x);
+	}
+	
+	pi = sum * W;
+	
+	end = omp_get_wtime();
+	
+	printf("The value of PI is %lf.\n", pi);
+	printf("The time done is %.4lf seconds\n", end - start);
+	
+	return 0;
+}
